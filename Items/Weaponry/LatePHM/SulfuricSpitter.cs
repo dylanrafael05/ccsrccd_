bc9def2f50b3ef631f53e26cdb6fe8f72bf34e9a
Dylan Rafael <dylanrafael@aol.com>
2020-08-05 01:57:14 -0400

    restaging

--- Items/Weaponry/LatePHM/SulfuricSpitter.cs
+++ Items/Weaponry/LatePHM/SulfuricSpitter.cs
@@ -6,51 +6,49 @@ using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace ChemistryClass.Items.Weaponry.LatePHM {
-    public class SulfurRainStaff : ChemistryClassItem {
+    public class SulfuricSpitter : ChemistryClassItem {
 
         public override void SetStaticDefaults() {
 
             Tooltip.SetDefault(
 
-                "Lets the corrosive rains fall"
+                "Spews sulfuric flames onto enemies"
 
                 );
-
-            Item.staff[item.type] = true;
 
         }
 
         public override void SafeSetDefaults() {
 
-            item.damage = 16;
+            item.damage = 14;
             item.crit = 4;
             item.knockBack = 1;
 
-            item.width = 32;
-            item.height = 32;
+            item.width = 44;
+            item.height = 16;
 
-            item.useTime = 50;
-            item.useAnimation = 50;
-            item.UseSound = SoundID.Item21;
+            item.useTime = 15;
+            item.useAnimation = 15;
+            item.UseSound = SoundID.Item34;
             item.useStyle = ItemUseStyleID.HoldingOut;
             item.autoReuse = true;
 
             item.noMelee = true;
 
             item.rare = 3;
-            item.value = Item.buyPrice(0, 0, 50, 0);
+            item.value = Item.buyPrice(0, 0, 55, 0);
 
-            item.shoot = ModContent.ProjectileType<Projectiles.LatePHMFL.SulfurRain>();
-            item.shootSpeed = 1;
+            item.shoot = ModContent.ProjectileType<Projectiles.LatePHMFL.SulfurFlame>();
+            item.shootSpeed = 15;
 
-            minutesToDecay = 3f;
+            minutesToDecay = 2.8f;
 
-            minPurityMult = 0.8f;
-            maxPurityMult = 1.05f;
+            minPurityMult = 1.05f;
+            maxPurityMult = 0.8f;
 
             SetRefinementData(
 
-                (ModContent.ItemType<Placeable.Blocks.SulfurClump>(), 1 / 5f)
+                (ModContent.ItemType<Placeable.Blocks.SulfurClump>(), 1 / 6f)
 
                 );
 
@@ -62,8 +60,8 @@ namespace ChemistryClass.Items.Weaponry.LatePHM {
 
                             ModContent.TileType<Tiles.Multitiles.MetallurgyStandTile>(),
                             1,
-                            (ModContent.ItemType<Placeable.Blocks.SulfurClump>(), 15),
-                            (ItemID.IronBar, 4)
+                            (ModContent.ItemType<Placeable.Blocks.SulfurClump>(), 18),
+                            (ItemID.IronBar, 5)
 
             );
 
@@ -71,8 +69,8 @@ namespace ChemistryClass.Items.Weaponry.LatePHM {
 
                             ModContent.TileType<Tiles.Multitiles.MetallurgyStandTile>(),
                             1,
-                            (ModContent.ItemType<Placeable.Blocks.SulfurClump>(), 15),
-                            (ItemID.LeadBar, 4)
+                            (ModContent.ItemType<Placeable.Blocks.SulfurClump>(), 18),
+                            (ItemID.LeadBar, 5)
 
             );
 
@@ -80,21 +78,10 @@ namespace ChemistryClass.Items.Weaponry.LatePHM {
 
         public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack) {
 
-            for(int _ = 0; _ < Main.rand.Next(4, 7); _++) {
+            position.X += speedX * 4;
+            position.Y += speedY * 4;
 
-                Vector2 tpe = Main.screenPosition + Main.MouseScreen;
-                Vector2 tp = tpe + new Vector2(Main.rand.NextFloat(-25, 25), Main.rand.NextFloat(-25, 25));
-
-                Vector2 av = Vector2.UnitY.RotatedBy(Main.rand.NextFloat(-CCUtils.PI_FLOAT / 6, CCUtils.PI_FLOAT / 6));
-
-                Vector2 sp = tp - av * MathHelper.Clamp(Main.MouseScreen.Y, 200, float.MaxValue) * 1.1f;
-                Vector2 sv = av * Main.rand.NextFloat(17f, 24f);
-
-                int p = Projectile.NewProjectile(sp, sv, type, damage, knockBack, player.whoAmI);
-
-            }
-
-            return false;
+            return true;
 
         }
 
